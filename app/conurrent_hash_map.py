1a5b0ec8b538e5ce4ca8c3e1c9a524a4cce8c9a0
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-03-29 10:26:37 +0200

    Class for Concurrent Hash Map

---
+++ app/conurrent_hash_map.py
@@ -0,0 +1,37 @@
+"""Module for a thread-safe hash-map"""
+
+from threading import Lock
+import typing
+
+
+class ConcurrentHashMap:
+    """
+    A thread-safe dictionary (hash-map).
+    It uses a private mutex for accessing resources concurrently
+    """
+    def __init__(self):
+        self._lock = Lock()
+        self.dictionary = {}
+
+    def add(self, key, value):
+        """Add a 'key->value' entry in the dictionary"""
+        with self._lock:
+            self.dictionary[key] = value
+
+    def get(self, key):
+        """
+        Is given a 'key' and returns its corresponding 'value'.
+        Or None, if the dictionary doesn't contain it
+        """
+        with self._lock:
+            return self.dictionary.get(key, None)
+    
+    def delete(self, key):
+        """Remove a 'key' from the dictionary"""
+        with self._lock:
+            self.dictionary.pop(key)
+        
+    def contains(self, key) -> bool:
+        """Returns whether or not the dictionary contains a given key"""
+        with self._lock:
+            return key in self.dictionary
