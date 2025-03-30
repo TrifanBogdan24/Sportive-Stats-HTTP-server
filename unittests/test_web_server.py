597279e7d82fcf2113f916d7914efe4f68d4e167
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-03-30 22:16:38 +0300

    Unittests: mean_by_category

--- unittests/test_web_server.py
+++ unittests/test_web_server.py
@@ -87,7 +87,6 @@ class TestWebServer(unittest.TestCase):
         """
         Helper method to perform the test logic for computing the best 5 states.
         It is reusable across multiple test cases with different file paths.
-        Assumes the result and reference are already sorted in descending order.
         """
         data_ingestor = DataIngestor(data_file)
 
@@ -120,7 +119,6 @@ class TestWebServer(unittest.TestCase):
         """
         Helper method to perform the test logic for computing the global_mean 
         It is reusable across multiple test cases with different file paths.
-        Assumes the result and reference are already sorted in descending order.
         """
         data_ingestor = DataIngestor(data_file)
 
@@ -154,7 +152,6 @@ class TestWebServer(unittest.TestCase):
         """
         Helper method to perform the test logic for computing the diff_from_mean 
         It is reusable across multiple test cases with different file paths.
-        Assumes the result and reference are already sorted in descending order.
         """
         data_ingestor = DataIngestor(data_file)
 
@@ -169,6 +166,52 @@ class TestWebServer(unittest.TestCase):
         
         result = data_ingestor.compute_response_diff_from_mean(question)
 
+
+        d = DeepDiff(result, reference_result, math_epsilon=0.01)
+        self.assertTrue(d == {}, str(d))
+
+
+
+    def test_diff_from_mean_case_1(self):
+        self._test_diff_from_mean('unittests/sample_data_multiple_states.csv', 
+                               'unittests/tests/diff_from_mean/input/in-1.json', 
+                               'unittests/tests/diff_from_mean/output/out-1.json')
+
+    def test_diff_from_mean_case_2(self):
+        self._test_diff_from_mean('unittests/sample_data_multiple_states.csv', 
+                               'unittests/tests/diff_from_mean/input/in-2.json', 
+                               'unittests/tests/diff_from_mean/output/out-2.json')
+
+
+    def test_mean_by_category_case_1(self):
+        self._test_mean_by_category('unittests/sample_data_multiple_states.csv', 
+                               'unittests/tests/mean_by_category/input/in-1.json', 
+                               'unittests/tests/mean_by_category/output/out-1.json')
+
+    def test_mean_by_category_case_2(self):
+        self._test_mean_by_category('unittests/sample_data_multiple_states.csv', 
+                               'unittests/tests/mean_by_category/input/in-2.json', 
+                               'unittests/tests/mean_by_category/output/out-2.json')
+
+
+
+    def _test_mean_by_category(self, data_file, input_file, output_file):
+        """
+        Helper method to perform the test logic for computing the mean_by_category
+        It is reusable across multiple test cases with different file paths.
+        """
+        data_ingestor = DataIngestor(data_file)
+
+        # Load input JSON data
+        with open(input_file) as file:
+            request_data = json.load(file)
+        question = request_data["question"]
+        
+        # Load expected output JSON data
+        with open(output_file) as file:
+            reference_result = json.load(file)
+        
+        result = data_ingestor.compute_response_mean_by_category(question)
 
         d = DeepDiff(result, reference_result, math_epsilon=0.01)
         self.assertTrue(d == {}, str(d))
